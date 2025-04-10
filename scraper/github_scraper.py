2ae18ecb4984c9258d47a3c230fae6789273b6af
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-04-10 15:57:56 -0700

    Update github_scraper.py

--- scraper/github_scraper.py
+++ scraper/github_scraper.py
@@ -1,5 +1,3 @@
-
-
 import requests
 
 def scrape(url):
@@ -8,7 +6,7 @@ def scrape(url):
     words = []
 
     # Raw file support (ideal use case)
-    if "raw.githubusercontent.com" in url:
+    if "raw.githubusercontent.com" or "gist.githubusercontent.com" in url:
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -18,7 +16,6 @@ def scrape(url):
         except requests.RequestException as e:
             print(f"Failed to fetch raw GitHub file: {e}")
     else:
-        print("Non-raw URLs are not fully supported yet. Try linking to raw.githubusercontent.com.")
+        print("Non-raw URLs are not fully supported yet. Try linking to raw.githubusercontent.com or gist.githubusercontent.com.")
     
     return words
-
