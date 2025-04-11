bff4c0769b8255ea1ac81a777e21096c75e0f741
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-04-10 18:43:13 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='word-reaper',
+    version='1.0.0',
+    author='d4rkfl4m3z',
+    description='Reap & Forge Wordlists for Password Cracking',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+        'requests',
+        'beautifulsoup4'
+    ],
+    entry_points={
+        'console_scripts': [
+            'wordreaper=word_reaper.word_reaper:main'
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+)
+
