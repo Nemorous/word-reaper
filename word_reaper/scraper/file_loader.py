94bfc9dd4811053ddf4387cead6b01d1431fdf05
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-04-16 01:46:09 -0700

    Update file_loader.py

--- word_reaper/scraper/file_loader.py
+++ word_reaper/scraper/file_loader.py
@@ -1,10 +1,13 @@
+from tqdm import tqdm
+
 def load(file_path):
     print(f"Loading words from file: \n{file_path}")
     words = []
 
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
-            for line in f:
+            lines = f.readlines()
+            for line in tqdm(lines, desc="Reading lines", unit="line"):
                 words.extend(line.strip().split())
     except FileNotFoundError:
         print("File not found.")
@@ -12,4 +15,3 @@ def load(file_path):
         print(f"Error reading file: {e}")
 
     return words
-
