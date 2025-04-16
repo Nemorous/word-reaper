8e5946630b6db3177374f6813551722144ab727c
d4rkfl4m3z <129306246+Nemorous@users.noreply.github.com>
2025-04-16 01:46:34 -0700

    Update github_scraper.py

--- word_reaper/scraper/github_scraper.py
+++ word_reaper/scraper/github_scraper.py
@@ -1,17 +1,18 @@
 import requests
+from tqdm import tqdm
 
 def scrape(url):
     print(f"Scraping GitHub source: \n{url}")
     
     words = []
 
-    # Raw file support (ideal use case)
-    if "raw.githubusercontent.com" or "gist.githubusercontent.com" in url:
+    if "raw.githubusercontent.com" in url or "gist.githubusercontent.com" in url:
         try:
             response = requests.get(url)
             response.raise_for_status()
             content = response.text
-            for line in content.splitlines():
+            lines = content.splitlines()
+            for line in tqdm(lines, desc="Parsing GitHub lines", unit="line"):
                 words.extend(line.strip().split())
         except requests.RequestException as e:
             print(f"Failed to fetch raw GitHub file: {e}")
